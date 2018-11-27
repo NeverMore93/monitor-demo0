72821b86af2f43f50094b6659741a242a3d34449
yuanwent <1305882275@qq.com>
2018-11-27 19:50:43 +0800

    add dependences

---
+++ src/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Box } from 'grommet';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Box>
+                    hello new react
+                </Box>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Box } from 'grommet';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                <Box>
+                    hello new react
+                </Box>
+            </div>
+        );
+    }
+}
+
+export default Main;
