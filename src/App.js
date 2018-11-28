f75a069fb39553dfa36a1c4e6f0fed18411902d4
yuanwent <1305882275@qq.com>
2018-11-28 19:42:29 +0800

    add antd

--- src/App.js
+++ src/App.js
@@ -1,71 +1,62 @@
 import React, { Component } from 'react';
-import './App.css';
-import { Collapse, Navbar, NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    UncontrolledDropdown,
-    DropdownToggle,
-    DropdownMenu,
-    DropdownItem } from 'reactstrap';
+import './CSS/layout.css';
+import 'antd/dist/antd.css';
+// import { Collapse, Navbar, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-
-        this.toggle = this.toggle.bind(this);
-        this.state = {
-            isOpen: false
-        };
+    render() {
+        return (
+            <Layout>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} style={{ lineHeight: '64px' }} >
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                </Header>
+                <Layout>
+                    <Sider width={200} style={{ background: '#fff' }}>
+                        <Menu  mode="inline" defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} style={{ height: '100%', borderRight: 0 }} >
+                            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+                                <Menu.Item key="1">option1</Menu.Item>
+                                <Menu.Item key="2">option2</Menu.Item>
+                                <Menu.Item key="3">option3</Menu.Item>
+                                <Menu.Item key="4">option4</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+                                <Menu.Item key="5">option5</Menu.Item>
+                                <Menu.Item key="6">option6</Menu.Item>
+                                <Menu.Item key="7">option7</Menu.Item>
+                                <Menu.Item key="8">option8</Menu.Item>
+                            </SubMenu>
+                            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+                                <Menu.Item key="9">option9</Menu.Item>
+                                <Menu.Item key="10">option10</Menu.Item>
+                                <Menu.Item key="11">option11</Menu.Item>
+                                <Menu.Item key="12">option12</Menu.Item>
+                            </SubMenu>
+                        </Menu>
+                    </Sider>
+                    <Layout style={{ padding: '0 24px 24px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>Home</Breadcrumb.Item>
+                            <Breadcrumb.Item>List</Breadcrumb.Item>
+                            <Breadcrumb.Item>App</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight: 280 }}>
+                            Content
+                        </Content>
+                    </Layout>
+                </Layout>
+            </Layout>
+        );
     }
-    toggle() {
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
-  render() {
-        const {isOpen} = this.state;
-    return (
-      <div>
-          <div>
-              <Navbar color="light" light expand="md">
-                  <NavbarBrand href="/">Micro Focus</NavbarBrand>
-                  <NavbarToggler onClick={this.toggle} />
-                  <Collapse isOpen={isOpen} navbar>
-                      <Nav className="ml-auto" navbar>
-                          <NavItem>
-                              <NavLink href="/components/">Components</NavLink>
-                          </NavItem>
-                          <NavItem>
-                              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                          </NavItem>
-                          <UncontrolledDropdown nav inNavbar>
-                              <DropdownToggle nav caret>
-                                  Options
-                              </DropdownToggle>
-                              <DropdownMenu right>
-                                  <DropdownItem>
-                                      Option 1
-                                  </DropdownItem>
-                                  <DropdownItem>
-                                      Option 2
-                                  </DropdownItem>
-                                  <DropdownItem divider />
-                                  <DropdownItem>
-                                      Reset
-                                  </DropdownItem>
-                              </DropdownMenu>
-                          </UncontrolledDropdown>
-                      </Nav>
-                  </Collapse>
-              </Navbar>
-          </div>
-      </div>
-    );
-  }
 }
 
 export default App;
